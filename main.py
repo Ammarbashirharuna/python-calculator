09788eaca505884951470f091e93ad9434fafcf0
Ammar bashir haruna <ammarbashirharuna2020@gmail.com>
2024-08-04 19:31:28 +0100

    collecting user choice

--- main.py
+++ main.py
@@ -3,3 +3,12 @@ print("welcome to my python GUI calculator".title())
 name = input("Enter Your Name: ")
 print(f"welcome {name} which operation do you want perform".title())
 print("[1]add\n[2]subtract\n[3]multiply\n[4]divide\n[5]reminder".upper())
+def add_function(first_number,second_number):
+    add = first_number + second_number
+    return add
+
+
+choice = input()
+if choice == 1 :
+    print("welcome to add wizard",name)
+    
