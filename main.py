0b02b79c64ad8da5f0e41032e5204253356fd67f
Ammar bashir haruna <ammarbashirharuna2020@gmail.com>
2024-08-04 19:25:32 +0100

    adding choice to the user

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
 from datetime import date
 print("welcome to my python GUI calculator".title())
 name = input("Enter Your Name: ")
+print(f"welcome {name} which operation do you want perform".title())
+print("[1]add\n[2]subtract\n[3]multiply\n[4]divide\n[5]reminder".upper())
