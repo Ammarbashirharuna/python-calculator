80a766e37cbbeecaaf783d61cd300d2a20866ff9
Ammar bashir haruna <ammarbashirharuna2020@gmail.com>
2024-08-05 01:08:52 +0100

    subtract functionality

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ print("welcome to my python GUI calculator".title())
 name = input("Enter Your Name: ")
 print(f"welcome {name} which operation do you want perform".title())
 print("[1]add\n[2]subtract\n[3]multiply\n[4]divide\n[5]reminder".upper())
+##functions to handle mathmatic operation
 def add_function(first_number,second_number):
     add = first_number + second_number
     return add
@@ -18,17 +19,21 @@ def divide_function(first_num,second_num):
 def reminder_function(first_num,second_num):
     reminder = first_num % second_num
     return reminder
-
-
-
-
-
+#giving choice to a user
 choice = input()
 to_num = int(choice)
 if to_num == 1 :
     print("welcome to add wizard",name)
     first_num = input("Enter first num ".title())
     to_int = int(first_num)
-    second_num = input("enter second num".title())
+    second_num = input("enter second num ".title())
     to_integer = int(second_num)
     print(f"the sum of your numbers is {add_function(to_int,to_integer)}")
+elif to_num == 2:
+    print("welcome to subtract wizard".title())
+    first_num  = input("enter first num ".title())
+    to_int = int(first_num)
+    second_num = input("enter second num ".title())
+    to_integer = int(second_num)
+    print(f"the subtract of your numbers is {subtract_function(to_int,to_integer)}")
+
